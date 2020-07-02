83532daa47c67771be7c597bad7e30f556294733
GoOsTT <65498969+GoOsTT@users.noreply.github.com>
2020-07-02 17:05:58 +0200

    Gave searchbar a function
    Gave sb a function it now can get in infos and display by the yelp api

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,17 +3,18 @@ import './BusinessList.css';
 import Business from '../Business/Business'
 
 class BusinessList extends React.Component {
-    render () {
-        return (
-            <div className="BusinessList">
-             {
-                this.props.businesses.map((business => {
-                    return <Business business={business} />
-                }))
-             }
-            </div>
-        )
+    render() {
+      return (
+        <div className="BusinessList">
+          {
+            this.props.businesses.map(business => {
+              return <Business business={business} key={business.id} />
+            })
+          }
+        </div>
+      );
     }
-};
+  }
+  
+  export default BusinessList;
 
-export default BusinessList;
